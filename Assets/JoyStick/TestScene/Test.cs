927d2241a5aa8e6455bd32189ab4aefb88a316ff
DESKTOP-VPLIUN9\xxxpassenger <xxxpassenger@yeah.net>
2018-05-19 14:54:30 +0800

    no message

--- Assets/JoyStick/TestScene/Test.cs
+++ Assets/JoyStick/TestScene/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using ClientStructure;
 
@@ -7,19 +8,31 @@ public class Test : MonoBehaviour {
 
     // Use this for initialization
     private JoyStick joyStick;
-	void Start () {
+    private Toggle typeToggle;
+    private Text dirText;
+
+    private void Awake()
+    {
         joyStick = GameObject.Find("Canvas/JoyStick").GetComponent<JoyStick>();
+        typeToggle = GameObject.Find("Canvas/Toggle").GetComponent<Toggle>();
+        dirText = GameObject.Find("Canvas/Text/Text").GetComponent<Text>();
+    }
+
+    void Start () {
+
         //摇杆移动半径是中间圆按钮拖动范围，按下生效范围（通过改变JoyStick的RectTransform的大小改变）
         //摇杆最大移动半径 
-        joyStick.JoyStickMaxRadius = 200;
+        joyStick.JoyStickMaxRadius = 110;
         //摇杆最小生效半径
-        joyStick.JoyStickMinRadius = 20;
+        joyStick.JoyStickMinRadius = 0;
         //按动时中间按钮的颜色
-        joyStick.OnTouchTumbColor = Color.red;
+        joyStick.OnTouchTumbColor = new Color(1f, 0.588f, 0.008f);
         //摇杆动态还是静态
-        joyStick.JoyStickType = JoyStickType.Dynamic;
+        joyStick.JoyStickType = (typeToggle.isOn ? JoyStickType.Dynamic : JoyStickType.Static); //JoyStickType.Static;
         //摇杆方向改变时回调
         joyStick.OnJoyStickTouchChangeDir += TestJoyStick;
+        //
+        typeToggle.onValueChanged.AddListener(value=> ChangeJoyStickType(value));
     }
 	
 	// Update is called once per frame
@@ -30,6 +43,12 @@ public class Test : MonoBehaviour {
     private void TestJoyStick(JoyStickDirection joyStickDir)
     {
         //摇杆返回的方向：        None = 摇杆抬起,  Forward = 方向右,  Backward = 方向左,  Down = 方向下；
-        Debug.Log("joyStickDir: " + joyStickDir);
+        dirText.text = joyStickDir.ToString();
+        //Debug.Log("joyStickDir: " + joyStickDir);
+    }
+
+    private void ChangeJoyStickType(bool value)
+    {
+        joyStick.JoyStickType = (value ? JoyStickType.Dynamic : JoyStickType.Static);
     }
 }
