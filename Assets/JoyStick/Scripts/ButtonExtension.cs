27be8dd4c0d8794094325d708f4db647fe1180c0
DESKTOP-VPLIUN9\xxxpassenger <xxxpassenger@yeah.net>
2018-05-21 17:24:35 +0800

    ①添加长按按钮扩展 ②添加RectTransform扩展

---
+++ Assets/JoyStick/Scripts/ButtonExtension.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ClientStructure
+{
+    public class ButtonExtension : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+        private bool isPressDown = false;
+
+        public delegate void ButtonDown();
+
+        public event ButtonDown OnButtonDown;
+
+        public delegate void ButtonUp();
+
+        public event ButtonUp OnButtonUp;
+
+        public delegate void TouchExitButton();
+
+        public event TouchExitButton OnTouchExitButton;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            IsPressDown = true;
+
+            if (OnButtonDown != null)
+            {
+                OnButtonDown.Invoke();
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            IsPressDown = false;
+
+            if (OnButtonUp != null)
+            {
+                OnButtonUp.Invoke();
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            IsPressDown = false;
+
+            if (OnButtonUp != null)
+            {
+                OnButtonUp.Invoke();
+            }
+        }
+
+        public bool IsPressDown
+        {
+            get
+            {
+                return isPressDown;
+            }
+
+            set
+            {
+                isPressDown = value;
+            }
+        }
+    }
+}
